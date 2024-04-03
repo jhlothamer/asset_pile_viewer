3c8ebc21175da5e9717afc293085170dc7ef4495
Jason Lothamer <jhlothamer@gmail.com>
2024-04-03 09:28:05 -0400

    update about dialog

--- asset_pile_viewer_app/lib/features/about/about.dart
+++ asset_pile_viewer_app/lib/features/about/about.dart
@@ -1,7 +1,39 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:assetPileViewer/data/asset_pile_viewer_db_schema.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+
+const _contentWidth = 400.0;
+const _githubUrl = 'https://github.com/jhlothamer/asset_pile_viewer';
+
+void _showLicenseDialog(BuildContext context) async {
+  var licenseText = await rootBundle.loadString('assets/LICENSE');
+  if (!context.mounted) {
+    return;
+  }
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('License'),
+        content: SingleChildScrollView(
+          child: Text(licenseText),
+        ),
+        actions: [
+          TextButton(
+            child: const Text('Close'),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        ],
+      );
+    },
+  );
+}
+
+void _launchSourceCodeUrl() {
+  launchUrlString(_githubUrl);
+}
 
 void showAbout(BuildContext context) {
   showAboutDialog(
@@ -11,64 +43,51 @@ void showAbout(BuildContext context) {
     applicationLegalese: '\u{a9} 2024 Jason Lothamer',
     applicationIcon: Image.asset('assets/images/app_icon.png'),
     children: [
-      Flex(
-        direction: Axis.horizontal,
-        children: [
-          const Text('License:'),
-          InkWell(
-            child: const Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'GPL-3.0',
-                style: TextStyle(decoration: TextDecoration.underline),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            onTap: () async {
-              var licenseText = await rootBundle.loadString('assets/LICENSE');
-              if (!context.mounted) {
-                return;
-              }
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('License'),
-                    content: SingleChildScrollView(
-                      child: Text(licenseText),
-                    ),
-                    actions: [
-                      TextButton(
-                        child: const Text('Close'),
-                        onPressed: () => Navigator.of(context).pop(),
-                      )
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-        ],
+      const SizedBox(
+        height: 20,
       ),
-      Flex(
-        direction: Axis.horizontal,
-        children: [
-          const Text('Source Code:'),
-          InkWell(
-            child: const Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'https://github.com/jhlothamer/asset_pile_viewer',
-                style: TextStyle(decoration: TextDecoration.underline),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            onTap: () {
-              launchUrlString(
-                  'https://github.com/jhlothamer/asset_pile_viewer');
-            },
-          ),
-        ],
+      RichText(
+        text: TextSpan(children: [
+          const TextSpan(text: 'This software is licensed under '),
+          TextSpan(
+              text: 'GNU General Public License V3',
+              style: const TextStyle(
+                  color: Colors.blue, decoration: TextDecoration.underline),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => _showLicenseDialog(context)),
+          const TextSpan(text: '.'),
+        ]),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      RichText(
+        text: const TextSpan(children: [
+          TextSpan(text: 'A copy of the source code can be obtained at '),
+        ]),
+      ),
+      const SizedBox(
+        height: 10,
+      ),
+      RichText(
+        text: TextSpan(children: [
+          TextSpan(
+              text: _githubUrl,
+              style: const TextStyle(
+                  color: Colors.blue, decoration: TextDecoration.underline),
+              recognizer: TapGestureRecognizer()..onTap = _launchSourceCodeUrl),
+        ]),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      SizedBox(
+        width: _contentWidth,
+        child: RichText(
+          text: const TextSpan(
+              text:
+                  'To view packages used by this software and their licenses, click the View Licenses button below.'),
+        ),
       ),
     ],
   );
